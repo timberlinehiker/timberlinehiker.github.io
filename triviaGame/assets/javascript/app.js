1e0909ca4bf85f2c8169503b2c771a3fee928124
timberlinehiker <43160673+timberlinehiker@users.noreply.github.com>
2018-10-20 09:50:51 -0700

    trivia game

---
+++ triviaGame/assets/javascript/app.js
@@ -0,0 +1,9 @@
+// // java for this one will be the main part 
+// try to get a master function that drives other functions  the function that loops another function through an Array, like the budgeting example from saturday will be a good starting point for getting the answer array and the question array to correspond 
+
+
+// set up vars to hold all the various operating values
+// the question and answer arrays will be bulky
+// make a button 1, button 2, button 3, and button 4 for easier indexing of the answers
+
+
